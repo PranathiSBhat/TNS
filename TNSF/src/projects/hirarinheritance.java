d9eb50f84aebda3f739f43e59236634621814139
Pranathi <Pranathi@LAPTOP-PHHULOMV>
2024-07-08 17:38:21 +0530

    second commit

---
+++ TNSF/src/projects/hirarinheritance.java
@@ -0,0 +1,20 @@
+package projects;
+
+class Animal{
+	
+}
+class Tiger extends Animal{
+	
+}
+class Deer extends Animal{
+	
+}
+
+public class hirarinheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
