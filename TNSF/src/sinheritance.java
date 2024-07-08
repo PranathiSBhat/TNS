5842fd3888e66a269390ba0775d5de555c4fd15b
Pranathi <Pranathi@LAPTOP-PHHULOMV>
2024-07-08 17:29:42 +0530

    inheritance

---
+++ TNSF/src/sinheritance.java
@@ -0,0 +1,27 @@
+
+class Animal{
+	void eat() {
+		
+	}
+}
+class Dog extends Animal{
+	void bark() {
+		
+	}
+}
+
+public class sinheritance {
+
+	public static void main(String[] args) {
+		Animal a= new Animal();
+		a.eat();
+		
+		Dog d=new Dog();
+		d.eat();
+		d.bark();
+		
+		Animal a1=new Dog();
+		a1.eat();
+	}
+
+	}
